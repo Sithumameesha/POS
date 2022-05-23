e250861e8a0144e55cdc7a2142b2eb8783fdbb42
Sithumameesha <sithumameesha76@gmail.com>
2022-05-23 08:31:25 +0530

    Introducing The Singelton

--- Layered_Architecture/src/Dao/DAOFactory.java
+++ Layered_Architecture/src/Dao/DAOFactory.java
@@ -12,4 +12,22 @@ public class DAOFactory {
         return daoFactory;
 
     }
+    public enum DAOTypes{
+        CUSTOMER,ITEM,ORDER,ORDERDETAILS,QUERYDAO
+
+    }
+    public void getDAO(DAOTypes Types){
+        switch (Types){
+            case CUSTOMER:
+                return;
+            case ITEM:
+                return;
+            case ORDER:
+                return;
+            case ORDERDETAILS:
+                return;
+            case QUERYDAO:
+                return;
+        }
+    }
 }
